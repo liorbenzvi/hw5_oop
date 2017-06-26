d3a976567a68cdcd55a1d5d519a91ec32fd5236f
liorbenzvi <liorbenzvi1907@gmail.com>
2017-06-26 14:57:49 +0300

    finished IntListUtils – max and reversed

--- p1/IntListUtils.h
+++ p1/IntListUtils.h
@@ -2,11 +2,39 @@
 #define INTLISTUTILS
 #include "IntList.h"
 
+template<bool b, int A, int B>
+struct BIG {
+    constexpr static int big = A;
+};
 
-template<int H1, int... RS1>
-struct IntListUtils<IntList<>,IntList<H1,RS1...>> {
-    typedef IntList<H1,RS1...> list;
+template<int A, int B>
+struct BIG<false, A, B> {
+    constexpr static int big = B;
+};
 
+template<typename A>
+struct IntListUtils {
+    typedef IntList<>reversed;
+};
+
+
+template<int A>
+struct IntListUtils<IntList<A>> {
+    constexpr static int max = A;
+    typedef IntList<A>reversed;
+};
+
+template<int A, int B>
+struct IntListUtils<IntList<A, B>> {
+    constexpr static int max = BIG<(A>B),A,B>::big;
+    typedef IntList<B,A> reversed;
+};
+
+template<int A, int B, int... RS>
+struct IntListUtils<IntList<A,B,RS...>> {
+    constexpr static int max = IntListUtils<IntList<BIG<(A>B),A,B>::big,RS...>>::max;
+    typedef typename IntListUtils<IntList<RS...>>::reversed res;
+    typedef typename ConcatedIntLists< res, IntList<B,A>>::list reversed;
 };
 
 
